b168a8158119117d0c89e16b4db28a56eb713f9c
n-radovanovic <radovanovic2310@gmail.com>
2024-08-13 19:19:26 +0200

    testiranje

---
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            ['name' => 'Admin'],
+            ['name' => 'Ulogovan'],
+            ['name' => 'Neulogovan'],
+        ]);
+        
+        User::factory(10)->create();
+
+        
+        (new RunPlansSeeder())->run();
+        (new CommentsSeeder())->run();
+        (new RunParticipantSeeder())->run();
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
